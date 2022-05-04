ca3daaf6787e591f36ca2689bcd5ad1710808aa3
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2022-05-04 16:07:19 -0300

    Initial commit

---
+++ Assets/CloneObjects.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloneObjects : MonoBehaviour
+{
+    public GameObject objectToClone;
+
+    public void InstantiateObject() {
+        Instantiate(objectToClone);
+    }
+
+}
