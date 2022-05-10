0085533801dad28a5b15f6dfbc851f348b227322
lucianojacofsky <101577485+lucianojacofsky@users.noreply.github.com>
2022-05-10 10:41:21 -0300

    .

---
+++ Assets/InputField.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputField : MonoBehaviour
+{
+    public InputField ifUserName;
+    public Text txtUserName;
+
+    public void UserGreetongs()
+    {
+        Debug.Log(int.Parse(txtUserName.text)*2);
+    }
+}
